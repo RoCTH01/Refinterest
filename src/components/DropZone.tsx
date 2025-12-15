132d48dc4f2543ac59305430cb139882df156298
RoCTH01 <cdrobby.chen@gmail.com>
2025-12-15 19:58:41 +0800

    explore drag and drop event

--- src/components/DropZone.tsx
+++ src/components/DropZone.tsx
@@ -1,10 +1,37 @@
 function DropZone({children}: {children: React.ReactNode}) {
 
     return (
-        <div className="drop-zone">
+        <div className="drop-zone"
+        onDragEnter={(e) => {
+            e.preventDefault();
+            console.log("Drag Entered Drop Zone",);
+        }}
+        onDragOver={(e) => {
+            e.preventDefault();
+            console.log("Dragging Over Drop Zone");
+        }}
+        onDrop={(e)=> {
+            e.preventDefault();
+            onFileDrop(e.dataTransfer.files);
+            console.log("Files Dropped in Drop Zone");
+        }}
+        onDragLeave={(e) => {
+            e.preventDefault();
+            console.log("Drag Left Drop Zone");
+        }}
+        >
             {children}
         </div>
     );
+}
+
+//handle file drop and upload logic here
+
+
+const onFileDrop = (files: FileList) => {
+
+    console.log("Files dropped:", files);
+    
 }
 
 export default DropZone;
